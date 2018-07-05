bcda48b9cc95c3129ee4809cc5fdece5574f3524
670312@cognizant.com <670312@cognizant.com>
2018-07-05 18:53:04 +0530

    added files

---
+++ AdditionProgram/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionProgram
+{
+    public class SampleProgram
+    {
+        public int sum(int n1, int n2)
+        {
+            int c;
+            c = n1 + n2;
+            return c;
+        }
+
+        public static void Main(string[] args)
+        {
+            SampleProgram sp = new SampleProgram();
+            Console.WriteLine("enter the number");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter the number");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("the number is:{0}", sp.sum(a, b));
+
+        }
+    }
+}
+
